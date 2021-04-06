787b37a2ed83d86fa609ebe8f2655e4f38c77e93
Minio10 <herminiom10@gmail.com>
2021-04-06 16:55:04 +0100

    added the hability to post new posts and to see them

--- network/models.py
+++ network/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length= 300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default = 0) # number of likes start at 0
--- network/views.py
+++ network/views.py
@@ -1,14 +1,28 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.urls import reverse
+from django import forms
 
-from .models import User
+
+from .models import User,Post
+
+class NewPostForm(forms.Form):
+
+    New_Post = forms.CharField(widget = forms.Textarea(
+        attrs = {
+            'class': 'form-control','cols': 5, 'rows': 3
+        }
+    ))
 
 
 def index(request):
-    return render(request, "network/index.html")
+    all_posts = Post.objects.all() #- means that its desceding order newer to older
+    all_posts = all_posts.order_by('-created_at')
+    return render(request, "network/index.html",{
+        "posts":all_posts,"form":NewPostForm()
+    })
 
 
 def login_view(request):
@@ -61,3 +75,25 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+def new_post(request):
+
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+
+        #Saves the post on the DB
+        if form.is_valid():
+            text = form.cleaned_data["New_Post"]
+
+
+            post = Post()
+            post.text = text
+            post.user = request.user
+
+            post.save()
+            return redirect("index")
+    #If there is an error the site should present error message
+    # return render(request,"auctions/error.html",{
+    #     "message": "Some problem happened while you submited your Comment"
+    # })
