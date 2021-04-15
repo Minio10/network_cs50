08660fb31a4add6a2844d2dc47145c24ce0c9752
Minio10 <herminiom10@gmail.com>
2021-04-15 19:16:58 +0100

    fixed the like button

--- network/static/network/posts.js
+++ network/static/network/posts.js
@@ -1,11 +1,10 @@
+
+var dislikes;
+var likes;
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
   var buttons = document.querySelectorAll(".edit");
-  var dislikes = document.querySelectorAll(".dislike");
-  var likes = document.querySelectorAll(".like");
-
-
 
   for (let i = 0, len = buttons.length; i < len; i++) {
     let temp = buttons[i].value;
@@ -15,23 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   }
 
-  for (let i = 0, len = dislikes.length; i < len; i++) {
-    let temp = dislikes[i].value;
-    dislikes[i].addEventListener("click", function(){
-      manage_likes(temp,1);
-    });
-
-  }
-
-  for (let i = 0, len = likes.length; i < len; i++) {
-    let temp = likes[i].value;
-    likes[i].addEventListener("click", function(){
-      manage_likes(temp,0);
-    });
-
-  }
-
-
 
   load_posts();
 
@@ -39,13 +21,35 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-function manage_likes(id,flag){
+function manage_likes(id, label){
 
   event.preventDefault();
 
+  // var flag;
+  var button;
+
+  if(label === "change_l"){
+
+    // flag = document.querySelector(`#dislike${id}`);
+    button = document.querySelector(`#change_l${id}`);
+
+
+  }
+
+  else if(label === "change_d"){
+    // flag = document.querySelector(`#like${id}`);
+    button = document.querySelector(`#change_d${id}`);
+
+  }
+
+  console.log(window.getComputedStyle(button, null).getPropertyValue("background-color"));
+
+
+
+
   var new_num_likes = document.querySelector(`#likes${id}`);
   //Unarchive
-  if(flag == 1){
+  if(window.getComputedStyle(button, null).getPropertyValue("background-color") === "rgb(255, 0, 0)"){
     fetch(`/manage_likes/${id}`,{
       method: 'PUT',
       body: JSON.stringify({
@@ -56,9 +60,11 @@ function manage_likes(id,flag){
     .then(result => {
 
       var likes = result["likes"];
-      var button = document.querySelector(`#change_l${id}`);
-      button.style.background = "white";
       new_num_likes.innerHTML = likes;
+      console.log(button);
+      button.style.background = "white";
+
+
 
     })
     .catch(err => console.log(err));
@@ -69,7 +75,7 @@ function manage_likes(id,flag){
 
   }
   //Archive
-  else if(flag == 0){
+  else if(window.getComputedStyle(button, null).getPropertyValue("background-color") === "rgb(255, 255, 255)"){
     fetch(`/manage_likes/${id}`,{
       method: 'PUT',
       body: JSON.stringify({
@@ -81,7 +87,7 @@ function manage_likes(id,flag){
 
       var likes = result["likes"];
       new_num_likes.innerHTML = likes;
-      var button = document.querySelector(`#change_d${id}`);
+      console.log(button);
       button.style.background = "red";
 
     })
@@ -92,6 +98,8 @@ function manage_likes(id,flag){
     },250);
 
   }
+
+
 
 
 
@@ -140,10 +148,14 @@ function send_edit_post(id){
 
 
 
+
+
 function load_posts(){
 
   // Show compose view and hide other views
   document.querySelector('#all_posts').style.display = 'block';
   document.querySelector('#edit_post').style.display = 'none';
+
+
 
 }
