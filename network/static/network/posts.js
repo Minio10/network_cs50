6342f4280b337717dcf20be54249b3b63a3078fb
Minio10 <herminiom10@gmail.com>
2021-04-17 17:06:34 +0100

    changed the js of the edit functionality, still refreshes after editing

--- network/static/network/posts.js
+++ network/static/network/posts.js
@@ -1,51 +1,20 @@
-
-var dislikes;
-var likes;
-document.addEventListener('DOMContentLoaded', function() {
-
-  // Use buttons to toggle between views
-  var buttons = document.querySelectorAll(".edit");
-
-  for (let i = 0, len = buttons.length; i < len; i++) {
-    let temp = buttons[i].value;
-    buttons[i].addEventListener("click", function(){
-      edit_post(temp);
-    });
-
-  }
-
-
-  load_posts();
-
-
-});
-
-
 function manage_likes(id, label){
 
   event.preventDefault();
 
-  // var flag;
   var button;
 
   if(label === "change_l"){
 
-    // flag = document.querySelector(`#dislike${id}`);
     button = document.querySelector(`#change_l${id}`);
 
 
   }
 
   else if(label === "change_d"){
-    // flag = document.querySelector(`#like${id}`);
     button = document.querySelector(`#change_d${id}`);
 
   }
-
-  console.log(window.getComputedStyle(button, null).getPropertyValue("background-color"));
-
-
-
 
   var new_num_likes = document.querySelector(`#likes${id}`);
   //Unarchive
@@ -106,56 +75,44 @@ function manage_likes(id, label){
 }
 
 
-function edit_post(id) {
+function send_edit_post(id){
 
-  event.preventDefault();
 
-  console.log(id);
 
+  //button of edit post
+  var button = document.querySelector('#send');
 
 
   // Show compose view and hide other views
   document.querySelector('#all_posts').style.display = 'none';
   document.querySelector('#edit_post').style.display = 'block';
 
-
-
   // Clear out composition fields
   document.querySelector('#compose-body').value = '';
 
-  // Form that sends the email info
-  document.querySelector('#compose-form').addEventListener('submit', () => send_edit_post(id));
+  send.addEventListener('click',() => {
+    fetch(`/edit_posts/${id}`,{
+      method: 'PUT',
+      body: JSON.stringify({
+        body: document.querySelector('#compose-body').value
 
-}
-
-
-function send_edit_post(id){
-
-
-
-  fetch(`/edit_posts/${id}`,{
-    method: 'PUT',
-    body: JSON.stringify({
-      body: document.querySelector('#compose-body').value
+      })
     })
-  })
-  setTimeout(function(){
-    load_posts();
-  },250);
+    var paragraph = document.querySelector(`#edit_p${id}`);
+    paragraph.innerHTML = document.querySelector('#compose-body').value;
+    console.log(paragraph.innerHTML);
+
+    // Show compose view and hide other views
+    document.querySelector('#all_posts').style.display = 'block';
+    document.querySelector('#edit_post').style.display = 'none';
+
+    // event.preventDefault();
 
 
-}
 
 
-
-
-
-function load_posts(){
-
-  // Show compose view and hide other views
-  document.querySelector('#all_posts').style.display = 'block';
-  document.querySelector('#edit_post').style.display = 'none';
-
+    return false;
+  });
 
 
 }
